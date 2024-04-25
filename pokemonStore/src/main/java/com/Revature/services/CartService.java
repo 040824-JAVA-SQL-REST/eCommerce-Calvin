99f19eec2d83fac37ad62d933373a317d1d9c31e
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2024-04-25 13:21:11 -0400

    added stuff to gitignore

---
+++ pokemonStore/src/main/java/com/Revature/services/CartService.java
@@ -0,0 +1,5 @@
+package com.Revature.services;
+
+public class CartService {
+    
+}
--- pokemonStore/src/main/java/com/Revature/services/StoreService.java
+++ pokemonStore/src/main/java/com/Revature/services/StoreService.java
@@ -1,5 +1,27 @@
 package com.Revature.services;
 
+import java.util.List;
+
+import com.Revature.daos.StoreDAO;
+import com.Revature.models.Store;
+
 public class StoreService {
-    
+
+    private StoreDAO storeDAO;
+    private final RoleService roleService;
+
+    public StoreService(RoleService roleService, StoreDAO storeDAO) {
+        this.storeDAO = storeDAO;
+        this.roleService = roleService;
+    }
+
+    public boolean isUnique(String name) {
+        List<Store> stores = storeDAO.findAll();
+        return stores.stream().noneMatch(s -> s.getName().equals(name));
+    }
+
+    public void save(Store store) {
+        
+    }
+
 }
