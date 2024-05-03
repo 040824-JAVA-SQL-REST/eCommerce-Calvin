eb911e20311765be210e74a0dc59c6d17aed2eee
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2024-05-03 00:58:09 -0400

    meh

--- pokemonStore/src/main/java/com/Revature/daos/OrderDAO.java
+++ pokemonStore/src/main/java/com/Revature/daos/OrderDAO.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.Revature.models.CartProduct;
 import com.Revature.models.Order;
 import com.Revature.utils.ConnectionFactory;
 
